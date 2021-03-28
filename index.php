71ddf4756a26e89d0ee6ecaef12de4ad7ce6cd3f
matias.bonet <matias.g.bonet@gmail.com>
2021-03-27 22:43:56 -0300

    labv01

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 3</title>
+</head>
+<body>
+    <div>
+        <h1>Indice de ejercicios de la materia Laboratorio 3</h1>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <a href="./Html">Html</a>
+            </li>
+            <li>
+                <a href="./Css">Css</a>
+            </li>
+            <li>
+                <a href="./Jscript">Jscript</a>
+            </li>
+            <li>
+                <a href="./Especiales">Especiales</a>
+            </li>
+            <li>
+                <a href="./Php">Php</a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
