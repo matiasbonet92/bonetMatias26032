44615fdf2fec9778affc41b289a406013e4c3ce6
matias.bonet <matias.g.bonet@gmail.com>
2021-07-12 21:15:38 -0300

    +cambios

--- Php/ejer30Server/destruirSesion.php
+++ Php/ejer30Server/destruirSesion.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if(!isset($_SESSION['identificativoDeSesion'])) {
+    if(!isset($_SESSION['usuario'])) {
         header('location: ./login.html');
         exit();
     }
